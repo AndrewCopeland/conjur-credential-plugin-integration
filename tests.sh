1e6c0151b649ed86f4b9ae2ef0fcb25cac7a1016
AndrewCopeland <andcope1995@gmail.com>
2020-03-06 10:07:48 -0500

    add tests for git integration in folder with no context

--- tests.sh
+++ tests.sh
@@ -112,12 +112,14 @@ start=$(date +%s)
 
 # run the tests
 # folder tests
-folder="team1"
-id_freestyle_secret=$(start_test "$folder" "freestyle-secret")
-id_freestyle_secret_username=$(start_test "$folder" "freestyle-secret-username")
-id_freestyle_secret_ssh_key=$(start_test "$folder" "freestyle-secret-ssh-key")
-id_freestyle_jit_secret=$(start_test "$folder" "freestyle-jit-secret")
-id_freestyle_jit_secret_username=$(start_test "$folder" "freestyle-jit-secret-username")
+team_folder="team1"
+id_freestyle_secret=$(start_test "$team_folder" "freestyle-secret")
+id_freestyle_secret_username=$(start_test "$team_folder" "freestyle-secret-username")
+id_freestyle_secret_ssh_key=$(start_test "$team_folder" "freestyle-secret-ssh-key")
+id_freestyle_jit_secret=$(start_test "$team_folder" "freestyle-jit-secret")
+id_freestyle_jit_secret_username=$(start_test "$team_folder" "freestyle-jit-secret-username")
+id_freestyle_git_no_context=$(start_test "$team_folder" "freestyle-git-no-context")
+
 
 # global tests
 id_freestyle_secret_global=$(start_test "" "freestyle-secret")
@@ -126,9 +128,9 @@ id_freestyle_secret_not_found_global=$(start_test "" "freestyle-secret-not-found
 id_freestyle_secret_ssh_key_global=$(start_test "" "freestyle-secret-ssh-key")
 
 # validate all of the tests
-validate_test "$folder" "freestyle-secret" "$id_freestyle_secret" "$GIT_ACCESS_TOKEN"
-validate_test "$folder" "freestyle-secret-username" "$id_freestyle_secret_username" "$GIT_ACCESS_TOKEN"
-validate_test "$folder" "freestyle-secret-ssh-key" "$id_freestyle_secret_ssh_key" "$GIT_ACCESS_TOKEN"
+validate_test "$team_folder" "freestyle-secret" "$id_freestyle_secret" "$GIT_ACCESS_TOKEN"
+validate_test "$team_folder" "freestyle-secret-username" "$id_freestyle_secret_username" "$GIT_ACCESS_TOKEN"
+validate_test "$team_folder" "freestyle-secret-ssh-key" "$id_freestyle_secret_ssh_key" "$GIT_ACCESS_TOKEN"
 
 # validate_test "$folder" "freestyle-jit-secret" "$id_freestyle_jit_secret" "$TEAM_SECRET"
 # validate_test "$folder" "freestyle-jit-secret-username" "$id_freestyle_jit_secret_username" "$TEAM_SECRET"
@@ -139,6 +141,7 @@ validate_test "" "freestyle-secret-ssh-key" "$id_freestyle_secret_ssh_key_global
 
 # validate expected failures
 validate_fail_test "" "freestyle-secret-not-found" "$id_freestyle_secret_not_found_global" "Variable 'does/not/exists' not found in account 'conjur'"
+validate_fail_test "$team_folder" "freestyle-git-no-context"  "$id_freestyle_git_no_context" "Unable to find credential at Global Instance Level"
 
 end=$(date +%s)
 duration=$((end - start))
@@ -156,4 +159,4 @@ else
   echo_red "FAILED! All tests did not pass!"
   exit 1
 fi
-
+curl -X POST -d '<jenkins><install plugin="git@latest" /></jenkins>' --header 'Content-Type: text/xml' http://localhost:8080/pluginManager/installNecessaryPlugins
